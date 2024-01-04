adc2871bf71413d84ba3b6bdec457d54e84d61d0
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-04 19:00:04 -0300

    Add files via upload

---
+++ tiposDados Python.py
@@ -0,0 +1,24 @@
+# Python = Linguagem de programação
+#Tipo de tipagem = Dinâmica / Forte
+#str -> string - > texto
+#Strings são textos que estão dentro de aspas""
+
+# Aspas simples
+
+print('Hello word')
+
+# Aspas duplas
+
+print("Hello word")#pyton não faz diferença de aspas simples ou aspas duplas sabe que e string
+
+#escape 
+
+print("Hello\" word")
+
+#r
+
+print("Hello\" word\"")#usado para expressão regulares em python
+
+#tipagem dinamica python ja sabe o tipo de dados que você estta passando 
+
+print(1234)
