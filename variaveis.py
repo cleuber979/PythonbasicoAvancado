5de395b221cdeae1482682e0b14d17d0a93f7a9a
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-04 14:53:11 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,22 @@
+#variaveis em python
+
+nome = "Cleuber"
+idade = 40
+altura = 1.85
+gostaGames= True
+Jogos = ["the last of us","uncharted","God of War","Hallo","Forza"]#array pyton #
+
+
+
+def soma(num1,num2):#funcao em python 
+    total = num1+num2
+    print("o valor e da soma das duas variaveis e ", + total)
+
+
+print(nome)
+print(idade)
+print(altura)
+print(gostaGames)
+print(Jogos)
+
+soma(4,5)
