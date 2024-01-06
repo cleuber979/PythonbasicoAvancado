c4d1b511088e78543396b1fc47ee9690075eb9e5
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-05 22:30:53 -0300

    Add files via upload

---
+++ type int float.py
@@ -0,0 +1,26 @@
+# Tipos int e Float 
+#int -> Numero inteiro
+#O tipo int representa qualquer numero
+#positivo ou negativo. int sem sinal e considerado positivo
+
+print(11) # int
+print (-11)# int 
+print(0) # int tipos primitivos 
+
+#folat -> Numero com ponto flutuante
+# O tipo float representa qualquer numero
+# positivo ou negativo comp ponto flutuante
+# float sem sinal e considerado positivo 
+print(1.1)
+#na programação sempre e usado ponto no numeros 
+print(1.1),10.11)
+print(0.0, -1.5)
+
+# A funcao type mostra o tipo que o pyton 
+#inferiu ao valor
+
+print( type('cleuber'))# classe type mostra o tipo de variavel 
+print( type(4)    )
+print( type(1.9) type(-2.9)type(0.0)    )
+
+
