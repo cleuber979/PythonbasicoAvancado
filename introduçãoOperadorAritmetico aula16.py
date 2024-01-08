217ea92c100f42517db23d48c7f1c02da49a4f6b
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-08 19:00:49 -0300

    Add files via upload

---
+++ introduçãoOperadorAritmetico aula16.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print('Adição ', adicao)
+
+subtracao = 10 - 5
+print('Subtração',subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação',multiplicacao)
+
+divisao = 20 / 4
+print('Divisão', divisao) # Sempre vai retornar uma divisão de ponto futoante
+
+divisao_inteira = 100 //2.2 #corta as cartas decimais 
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10 
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 #resto da divisao 
+print('Modulo',modulo)
+
+print(10 % 8 == 0)#quero saber se um numero e multiplo de 8
+print(16 % 8 ==0 )
+print(10% 2 == 0)# se um numero é par ou inpar
+print(15% 2 == 0)
+print(16% 2 == 0)
