867c08fe1f7978b922c413073c1064eed54b8550
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-08 20:31:41 -0300

    Update concatenacao aula 08.py

--- concatenacao aula 08.py
+++ concatenacao aula 08.py
@@ -6,4 +6,12 @@ tres_vezes_cleuber = 3 * 'cleuber'
 print(a_dez_vezes)
 print((tres_vezes_cleuber)
 
+# 1. (n+n) oredm das sequencias matematicas
+#2. **
+#3. * / // %
+#4. + -
+
+conta_1 = (1 + int(0.5 + 0.5) ** (5+5))
+print(conta_1)
+
 
