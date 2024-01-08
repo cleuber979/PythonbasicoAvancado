09d60a8e568e76bb48ee3cd1e3f9fa08b061edf7
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-08 19:14:23 -0300

    Add files via upload

---
+++ concatenacao aula 08.py
@@ -0,0 +1,9 @@
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 # pode fazer a repetição 
+tres_vezes_cleuber = 3 * 'cleuber'  
+print(a_dez_vezes)
+print((tres_vezes_cleuber)
+
+
