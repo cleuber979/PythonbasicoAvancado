737824962c138f8e9d3cf3f6ccb85a2332d5da52
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-08 23:07:20 -0300

    Add files via upload

---
+++ formatacao-Strings_parte1.py
@@ -0,0 +1,13 @@
+nome = 'cleuber maciel '
+altura = 1.80
+peso = 105
+imc = peso/altura**2
+
+"f-strings"
+linha_1=f'{nome} tem a {altura} de altura '
+linha_2=f'pesa {peso} e seu IMC e de '
+linha_3=f'{imc:.2f}'
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
