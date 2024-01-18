aa31bcf49808cc6d18a6660a4de5dd7b8953d40c
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-18 00:03:13 -0300

    Add files via upload

---
+++ Introducao_IF_elif_else.py
@@ -0,0 +1,12 @@
+# if / elif...../ else
+#se  /se não se/ se não
+
+entradaDados= input('Você quer entrar ou sair do sistema? Por favor digite entrar ou sair ')
+
+if entradaDados== 'entrar':
+    print('Você acaba de entrar no sistema usuário logado')
+elif entradaDados=='sair':
+    print('você saiu do sistema obrigado! ')
+else:
+    print('Por favor digite entrar ou sair para ter acesso: ')    
+
