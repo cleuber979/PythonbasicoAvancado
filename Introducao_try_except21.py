e09216e0b43c9cf9538b8238273d3f91e8517d92
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-02-16 01:47:44 -0300

    Update Introducao_try_except21.py

--- Introducao_try_except21.py
+++ Introducao_try_except21.py
@@ -1,3 +1,4 @@
+
 #Introdução ao try/execept
 #try -> tenta execultar o codigo 
 #except -> ocorreu algum erro ao tentar executar
@@ -9,7 +10,15 @@
 
 numero = input('Vou dobrar o numero que vc digitar : ')
 
+try: 
+    print('STR:',numero)
+    numero_float = float(numero)
+    print('FLOAT:',numero_float)
+    print(f'O dobro de {numero} é {numero_float * 2}')
+except:
+    print('O valor digitado não é um numero')
 
-numero_float = float(numero)
-print(numero.isdigit())
-print(f'O dobro de {numero} é {numero_float * 2}')
+
+#numero_float = float(numero)
+#print(numero.isdigit())
+#print(f'O dobro de {numero} é {numero_float * 2}')
